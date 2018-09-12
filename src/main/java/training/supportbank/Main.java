4aaa4c05cda4b7ab71effc5fe54d0ea7c78e5f67
Jess <jessica.gilbert@softwire.com>
2018-09-12 15:14:24 +0100

    working

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -5,39 +5,48 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String args[]) throws IOException {
-        // Your code here!
+    public static void main(String[] args) throws IOException {
+
         Path path = Paths.get("C:\\Users\\JJG\\Work\\Training\\SupportBank-Java-Template\\2Transactions2014.csv");
         List<String> lines = Files.readAllLines(path);
 
-        Scanner scanner = new Scanner(System.in);
-        String nextLine = scanner.nextLine();
-
-        String regex = "[A-Za-z]*";
+//        byte[] bytes = Files.readAllBytes(path);
+//        String fileContents =  new String(bytes);
 
 
-        String[] header = nextLine.split(regex);
-        System.out.println(Arrays.toString(header));
+//        Pattern p = Pattern.compile("(.*),(.*),(.*),(.*),(.*)");
+//        Matcher m = p.matcher(fileContents);
 
-        while (scanner.hasNext()) {
-            String[] row = scanner.nextLine().split(regex);
+        for (String line: lines) {
 
-            System.out.println(Arrays.toString(row));
-            System.out.println(row[0]);
+            String[] bits = line.split(",");
+
+            String transDate = bits[0];
+            String fromName = bits[1];
+            String toName = bits[2];
+            String narrative = bits[3];
+            String transAmount = bits[4];
+
+            System.out.println(bits[2]);
+
+
+
         }
-        }
+
+
+//        System.out.println(fileContents);
     }
-
-
-
 }
+
+
+
+
+
 
