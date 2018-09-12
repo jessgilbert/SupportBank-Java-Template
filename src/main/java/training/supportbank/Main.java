dfbadf157167a8fbb7321968d4ee7831cfdf77eb
Jess <jessica.gilbert@softwire.com>
2018-09-12 13:50:11 +0100

    edits

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,43 @@
 package training.supportbank;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
-    public static void main(String args[]) {
+
+    public static void main(String args[]) throws IOException {
         // Your code here!
-        System.out.println("Test!");
+        Path path = Paths.get("C:\\Users\\JJG\\Work\\Training\\SupportBank-Java-Template\\2Transactions2014.csv");
+        List<String> lines = Files.readAllLines(path);
+
+        Scanner scanner = new Scanner(System.in);
+        String nextLine = scanner.nextLine();
+
+        String regex = "[A-Za-z]*";
+
+
+        String[] header = nextLine.split(regex);
+        System.out.println(Arrays.toString(header));
+
+        while (scanner.hasNext()) {
+            String[] row = scanner.nextLine().split(regex);
+
+            System.out.println(Arrays.toString(row));
+            System.out.println(row[0]);
+        }
+        }
     }
+
+
+
 }
+
