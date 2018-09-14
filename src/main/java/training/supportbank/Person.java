b043c2e38f56697d1a763dc317aab65d8fd95fda
Jess <jessica.gilbert@softwire.com>
2018-09-14 08:27:39 +0100

    Made a start on adding transaction amounts to people

--- src/main/java/training/supportbank/Person.java
+++ src/main/java/training/supportbank/Person.java
@@ -2,13 +2,14 @@ package training.supportbank;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Person {
     public String name;
     public List<Transaction> transactions = new ArrayList<>();
 
-    public Person(String name){
+    public Person(String name) {
         this.name = name;
     }
 
@@ -16,12 +17,28 @@ public class Person {
         return name + transactions + "\n";
     }
 
-    public String getSummary(){
-        Double total = 0
-        for (Transaction currentTransaction: transactions) {
+    public String getSummary(List<Transaction> transactions, HashMap<String, Person> hm) {
+        Double total = 0.0;
+        for (Transaction currentTransaction : transactions) {
 
-            if()
+            if (name.equals(currentTransaction.fromName)); {
+                total - currentTransaction.transAmount;
+            }
+            else if (name.equals(currentTransaction.toName)) {
+                total + currentTransaction.transAmount;
+            }
         }
+        if(total > 0 ) {
+            return name + " is owed " + total + "\n:" ;
+        }
+        else if(total < 0 ) {
+            return name + " owes " + total + "\n:";
+        }
+        else {
+            return name + "isnt owed nor owes any amount" + "\n:";
+        }
+
 
     }
 }
+
